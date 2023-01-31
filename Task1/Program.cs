303ddda64b9e8f67f64402c0bdbe1ede64d9f830
Dinara <ash.solarium@gmail.com>
2023-01-31 11:47:06 +0300

    done

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+
+void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 11); // [1, 10]
+}
+
+
+void ReplaceElement(int[] array)
+{
+  for (int i = 0; i < array.Length / 2; i++)
+  {
+    int temp = array[i];
+    array[i] = array[array.Length - i - 1];
+    array[array.Length - i - 1] = temp;
+  }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный масссив: [{string.Join(", ", array)}]");
+ReplaceElement(array);
+Console.WriteLine($"Конечный масссив: [{string.Join(", ", array)}]");
---
+++ Task40/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с 
+// сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+
+Console.Clear();
+Console.Write("Введите строну а: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите строну b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите строну c: ");
+double c = Convert.ToDouble(Console.ReadLine());
+if ((a + b > c) && (b + c > a) && (a + c > b))
+{
+    Console.WriteLine("Треугольник");
+}
+else
+    Console.WriteLine("Не треугольник");
